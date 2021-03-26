3dc427b17a890ec1eec0d7437c808682ef563f0a
Jelani Thomas <jelani13@icloud.com>
2021-03-26 02:52:44 -0400

    added routes to server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const morgan = require("morgan");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname, "public"));
 
 // Bring in mongoose.connect
 
-//Declare routes
-
-//Declare API routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`My app is running on ${PORT}`);
