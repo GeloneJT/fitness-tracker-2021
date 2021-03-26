c00d04e5e9f1f6336b94bf57dbafbd3920433e38
Jelani Thomas <jelani13@icloud.com>
2021-03-26 06:32:30 -0400

    server.js setup completed

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname, "public"));
+app.use(express.static("public"));
 
-// Bring in mongoose.connect
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
