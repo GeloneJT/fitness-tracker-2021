5c1c6c14f0b41ddea71d72b5462ccb1a79a22400
Jelani Thomas <jelani13@icloud.com>
2021-03-28 17:05:54 -0400

    forgot filter in findByIdAndUpdate

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,7 @@ router.get("api/workouts/range", (req, res) => {
 
 router.put("api/workouts/:id", (req, res) => {
   Workout.findByIdAndUpdate(
-    {},
+    {_id: req.params.id},
     { $push: { exercises: req.body } },
     { new: true }
   )
