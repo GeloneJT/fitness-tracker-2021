2b6db553665ac5c5783a239dc95b495718266aa9
Jelani Thomas <jelani13@icloud.com>
2021-03-28 01:09:51 -0400

    localhost working to route to exercise.html now

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,30 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const mongoose = require("mongoose");
 
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+//Get workouts by ID
+router.get("api/workouts/:id", (req, res) => {
+  Workout.findById(req.params.id).then()
+})
+
+//Look up  .aggregate for Mongoose
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      return res.json(true);
+    })
+    .catch((err) => {
+      res.json(400).json(err);
+    });
+});
 module.exports = router;
--- routes/html.js
+++ routes/html.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
+const path = require("path");
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/excerise.html"));
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
